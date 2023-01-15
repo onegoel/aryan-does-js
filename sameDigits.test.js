73ef5036d4affa487029d679fc58b8fadc888ee4
Aryan Goel <aryangoel6900@yahoo.in>
2023-01-15 23:18:56 +0530

    test: test cases for isMonoDigit() that checks if a given num is composed of repeated digits only

---
+++ sameDigits.test.js
@@ -0,0 +1,20 @@
+const isMonoDigit = require('./sameDigits')
+
+describe('tests for isMonoDigit() that checks if a given num is composed of repeated digits only (monodigit or not)', () => {
+
+    test("should return true if given num is 3333", () => {
+        expect(isMonoDigit(3333)).toBe(true);
+    });
+
+    test("should return false if given num is 3", () => {
+        expect(isMonoDigit(3)).toBe(false);
+    });
+
+    test("should return false if given num is 232", () => {
+        expect(isMonoDigit(true)).toBe(false);
+    });
+
+    test("should return true if given num is -3333", () => {
+        expect(isMonoDigit(-3333)).toBe(true);
+    });
+})
