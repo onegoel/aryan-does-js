53f9b37db92b80516d569f75e20faf49b1735b8a
Aryan Goel <aryangoel6900@yahoo.in>
2023-01-15 23:06:16 +0530

    fix(test): change test case descriptions for last 2 test cases

--- numVowels.test.js
+++ numVowels.test.js
@@ -10,11 +10,11 @@ describe('tests for numVowels() that counts num of vowels in a str', () => {
         expect(numVowels('Aryan Goel')).toBe(4);
     });
     
-    test("should return 0 if given arr is 'YzyZ'", () => {
+    test("should return 0 if given str is 'YzyZ'", () => {
         expect(numVowels('YzyZ')).toBe(0);
     });
 
-    test("should return 1 if given arr is '!%^&(u)'", () => {
+    test("should return 1 if given str is '!%^&(u)'", () => {
         expect(numVowels('!%^&(u)')).toBe(1);
     });
 })
