a965666248f99a7125b354bfcc7440001351c06f
Aryan Goel <aryangoel6900@yahoo.in>
2023-01-15 13:17:40 +0530

    feat: 2 functions (arrow and regular) to check if a str ends with 'Script'

---
+++ endsWith.js
@@ -0,0 +1,29 @@
+// Write a JavaScript function ( both regular and arrow function) which takes a string as parameter to 
+// test whether a string end with "Script"(case-sensitive). The string length must be greater or equal to 6.
+
+// arrow
+isEndingScript = (str) => {
+    if (str.length <= 6) {
+        return false;
+    }
+    else {
+        return str.toString().endsWith('Script');
+        // return str.toString().trim().endsWith('Script');
+    }
+}
+
+/*
+// regular
+function isEndingScript (str) {
+    if (str.length <= 6) {
+        return false;
+    }
+    else {
+        return str.toString().endsWith('Script');
+        // return str.toString().trim().endsWith('Script');
+    }
+}
+*/
+console.log(isEndingScript('there is no Script'));
+console.log(isEndingScript('there is no Script '));
+
